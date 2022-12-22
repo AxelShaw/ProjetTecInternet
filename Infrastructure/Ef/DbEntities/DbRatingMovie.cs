64649d4970db359f937a630db9d3281c99eb99d0
AxelShaw <114676277+AxelShaw@users.noreply.github.com>
2022-12-22 16:14:21 +0100

    change int -> decimal for rating movie

--- Infrastructure/Ef/DbEntities/DbRatingMovie.cs
+++ Infrastructure/Ef/DbEntities/DbRatingMovie.cs
@@ -7,7 +7,7 @@ namespace Infrastructure.Ef.DbEntities;
 public class DbRatingMovie
 {
 
-    public int Average_rating { get; set; }
+    public decimal Average_rating { get; set; }
     public int NumVote { get; set; }
     
     [Key]
