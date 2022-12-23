35eed67823493625e0cf99796df1e1bca54468c1
MaadidMaxence <117234764+MaadidMaxence@users.noreply.github.com>
2022-12-23 17:13:36 +0100

    Test unitaire FormatDate actu

--- Domain/Actu.cs
+++ Domain/Actu.cs
@@ -1,4 +1,7 @@
-﻿namespace Domain;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Domain;
 
 public class Actu
 {
@@ -6,4 +9,14 @@ public class Actu
     public int IdMovieRef { get; set; }
     public string NewsActu { get; set; }
     public string Release_actu { get; set; }
+
+    public bool IsValidateDate(string releaseDate)
+    {
+        /*bool pattern  = DateTime.TryParseExact(actu.Release_actu, "MM/dd/yyyy",
+            CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+        
+        return Regex.IsMatch(releaseDate, pattern);*/
+        
+        return DateTime.TryParseExact(releaseDate, "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+    }
 }
---
+++ Unit test/TestDateReleaseActu.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using Domain;
+using NUnit.Framework;
+
+namespace Unit_test;
+
+public class TestDateReleaseActu
+{
+    [Test]
+    public void TestReleaseActuFormat_Ok()
+    {
+        // Arrange
+        Actu actu = new Actu { Release_actu = "12/23/2022" };
+
+        // Actu
+        var result = actu.IsValidateDate(actu.Release_actu);
+        
+        
+        // Assert
+        Assert.IsTrue(result);
+    }
+    
+    [Test]
+    public void TestReleaseActuFormat_NotOk()
+    {
+        // Arrange
+        Actu actu = new Actu { Release_actu = "19/87/1987" };
+
+        // Actu
+        var result = actu.IsValidateDate(actu.Release_actu);
+        
+        
+        // Assert
+        Assert.IsFalse(result);
+    }
+    
+    
+}
